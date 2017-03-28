61d3be4e70d5795e3e93cd9fcc8249e83b03c5f8
chungyihung <chungyihung1988@gmail.com>
2017-03-29 01:08:56 +0800

    Implement transform method between xlsx and DB
    It's a rough version to try the functionality.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 #!/usr/local/bin/python3
 import member as mbr
 import tkinter as tk
-import pygubu
 
 """
 Main window settings
 """
-MainWindow_W = 640
+MainWindow_W = 720
 MainWindow_H = 480
 
-MainFrame_W = 500
-MainFrame_H = 400
+MenuFrame_W = MainWindow_H
+MenuFrame_H = 100
+
+MainFrame_W = MainWindow_H
+MainFrame_H = MainWindow_H - MenuFrame_H
 
 """
 Main window class
@@ -22,51 +24,170 @@ class Application:
         self.curr_mbr = mbr.member()
 
         self.master = master
+        self.menuFrame = tk.Frame( self.master, width = MenuFrame_W, height = MenuFrame_H )
+        self.menuFrame.grid( row = 0, column = 0 )
+        self.MenuHandler( self.menuFrame )
+
         self.mainFrame = tk.Frame( self.master, width = MainFrame_W, height = MainFrame_H )
-        self.mainFrame.pack( fill=tk.BOTH)
+        self.mainFrame.grid( row = 1, column = 0 )
 
-        self.NameLebel = tk.Label( self.mainFrame, text = "Name: " ).grid( row = 1 , column = 0)
-        self.IDLebel = tk.Label( self.mainFrame, text = "ID: " ).grid( row = 2 , column = 0)
-        self.PhoneLebel = tk.Label( self.mainFrame, text = "Phone: " ).grid( row = 3 , column = 0)
+        self.gui_pg_insert_member( self.mainFrame )
 
-        self.NameEntry_Var  = tk.StringVar()
-        self.IDEntry_Var    = tk.IntVar()
-        self.PhoneEntry_Var = tk.StringVar()
-        self.QueryID_Var    = tk.IntVar()
+    def MenuHandler( self, frame ):
+        """         Constant        """
+        BTN_MBR_INFO_PG_STR     = "會員資料"
+        BTN_STATISTIC_PG_STR    = "統計資料"
+        BTN_GROUP_PG_STR        = "分組名單"
+        BTN_VOLUNTEER_PG_STR    = "馬拉松志工調度"
 
-        self.NameEntry = tk.Entry( self.mainFrame, textvariable = self.NameEntry_Var ).grid( row = 1, column = 1)
-        self.IDEntry = tk.Entry( self.mainFrame, textvariable = self.IDEntry_Var ).grid( row = 2, column = 1)
-        self.PhoneEntry = tk.Entry( self.mainFrame, textvariable = self.PhoneEntry_Var ).grid( row = 3, column = 1)
+        """         Menu Setting    """
+        self.btn_mbr_info_pg    = tk.Button( frame, text = BTN_MBR_INFO_PG_STR  )
+        self.btn_statistic_pg   = tk.Button( frame, text = BTN_STATISTIC_PG_STR )
+        self.btn_group_pg       = tk.Button( frame, text = BTN_GROUP_PG_STR     )
+        self.btn_volunteer_pg   = tk.Button( frame, text = BTN_VOLUNTEER_PG_STR )
 
-        self.queryidLabel = tk.Label( self.mainFrame, text = "Search ID: " ).grid( row = 0, column = 0 )
-        self.queryidEntry = tk.Entry( self.mainFrame, textvariable = self.QueryID_Var ).grid( row = 0, column = 1 )
-        self.queryidBtn = tk.Button( self.mainFrame, text = "Search", command = self.queryMember ).grid( row = 0, column = 2 )
+        self.btn_mbr_info_pg.grid(  row = 0, column = 0)
+        self.btn_statistic_pg.grid( row = 0, column = 1)
+        self.btn_group_pg.grid(     row = 0, column = 2)
+        self.btn_volunteer_pg.grid( row = 0, column = 3)
 
-        self.saveCurrentBtn = tk.Button( self.mainFrame, text = "Save", command = self.SaveMemberInDB ).grid( row = 0, column = 3 )
+    def gui_pg_member_table( self, frame ):
+        print( "Currently not supported..." )
 
-        self.GUI_fill_entry()
+    def gui_pg_insert_member(self, frame ):
+        """
+        TODO: Use an general array and dict to organize widgets and their attributes
+              and create them in an iteration manner.
+        """
+        self.PositionEntry_Var      = tk.StringVar()
+        self.NameEntry_Var          = tk.StringVar()
+        self.IDCardEntry_Var        = tk.StringVar()
+        self.BorthYearROCEntry_Var  = tk.StringVar()
+        self.BorthYMDEntry_Var      = tk.StringVar()
+        self.AreaEntry_Var          = tk.StringVar()
+        self.CellPhoneEntry_Var     = tk.StringVar()
+        self.PhonePrimaryEntry_Var  = tk.StringVar()
+        self.PhoneSecondEntry_Var   = tk.StringVar()
+        self.AddressEntry_Var       = tk.StringVar()
+
+        self.QueryID_Var            = tk.IntVar()
+
+        self.PositionLabel      = tk.Label( frame, text = "Position: " )
+        self.NameLabel          = tk.Label( frame, text = "Name: " )
+        self.IDCardLabel        = tk.Label( frame, text = "ID Card: " )
+        self.BorthYearROCLabel  = tk.Label( frame, text = "Birth( Republic Era): " )
+        self.BorthYMDLabel      = tk.Label( frame, text = "Birth: " )
+        self.AreaLabel          = tk.Label( frame, text = "Area: " )
+        self.CellPhoneLabel     = tk.Label( frame, text = "Cell Phone: " )
+        self.PhonePrimaryLabel  = tk.Label( frame, text = "Primary Phone: " )
+        self.PhoneSecondLabel   = tk.Label( frame, text = "Secondary Phone: " )
+        self.AddressLabel       = tk.Label( frame, text = "Address: " )
+
+        self.queryidLabel       = tk.Label( frame, text = "Search ID: " )
+
+        self.PositionEntry      = tk.Entry( frame, textvariable = self.PositionEntry_Var        )
+        self.NameEntry          = tk.Entry( frame, textvariable = self.NameEntry_Var            )
+        self.IDCardEntry        = tk.Entry( frame, textvariable = self.IDCardEntry_Var          )
+        self.BorthYearROCEntry  = tk.Entry( frame, textvariable = self.BorthYearROCEntry_Var    )
+        self.BorthYMDEntry      = tk.Entry( frame, textvariable = self.BorthYMDEntry_Var        )
+        self.AreaEntry          = tk.Entry( frame, textvariable = self.AreaEntry_Var            )
+        self.CellPhoneEntry     = tk.Entry( frame, textvariable = self.CellPhoneEntry_Var       )
+        self.PhonePrimaryEntry  = tk.Entry( frame, textvariable = self.PhonePrimaryEntry_Var    )
+        self.PhoneSecondEntry   = tk.Entry( frame, textvariable = self.PhoneSecondEntry_Var     )
+        self.AddressEntry       = tk.Entry( frame, textvariable = self.AddressEntry_Var         )
+
+        self.queryidEntry       = tk.Entry( frame, textvariable = self.QueryID_Var )
+        self.queryidEntry.bind('<Return>', ( lambda event: self.queryMember() ) )
+
+        self.queryidBtn         = tk.Button( frame, text = "Search", command = self.queryMember )
+        self.saveCurrentBtn     = tk.Button( frame, text = "Save", command = self.SaveMemberInDB )
+
+        """ Test function button """
+        self.cnvt_excel_to_db = tk.Button( frame, text = "Excel to DB", command = self.curr_mbr.cnvt_excel_to_db )
+        self.cnvt_db_to_excel = tk.Button( frame, text = "DB to Excel", command = self.curr_mbr.cnvt_db_to_excel )
+
+        """ Organize the widgets """
+        self.queryidLabel.grid(     row = 0, column = 0 )
+        self.queryidEntry.grid(     row = 0, column = 1 )
+
+        self.PositionLabel.grid(        row = 1,  column = 0 )
+        self.NameLabel.grid(            row = 2,  column = 0 )
+        self.IDCardLabel.grid(          row = 3,  column = 0 )
+        self.BorthYearROCLabel.grid(    row = 4,  column = 0 )
+        self.BorthYMDLabel.grid(        row = 5,  column = 0 )
+        self.AreaLabel.grid(            row = 6,  column = 0 )
+        self.CellPhoneLabel.grid(       row = 7,  column = 0 )
+        self.PhonePrimaryLabel.grid(    row = 8,  column = 0 )
+        self.PhoneSecondLabel.grid(     row = 9,  column = 0 )
+        self.AddressLabel.grid(         row = 10, column = 0 )
+
+        self.PositionEntry.grid(        row = 1,  column = 1 )
+        self.NameEntry.grid(            row = 2,  column = 1 )
+        self.IDCardEntry.grid(          row = 3,  column = 1 )
+        self.BorthYearROCEntry.grid(    row = 4,  column = 1 )
+        self.BorthYMDEntry.grid(        row = 5,  column = 1 )
+        self.AreaEntry.grid(            row = 6,  column = 1 )
+        self.CellPhoneEntry.grid(       row = 7,  column = 1 )
+        self.PhonePrimaryEntry.grid(    row = 8,  column = 1 )
+        self.PhoneSecondEntry.grid(     row = 9,  column = 1 )
+        self.AddressEntry.grid(         row = 10, column = 1 )
+
+        self.queryidBtn.grid(       row = 0, column = 2 )
+        self.saveCurrentBtn.grid(   row = 0, column = 3 )
+        self.cnvt_excel_to_db.grid( row = 0, column = 4 )
+        self.cnvt_db_to_excel.grid( row = 0, column = 5 )
+
+        self.GUI_blank_entry()
 
     """
     Need to wrap sqlite to fill up the data item
     """
     def GUI_fill_entry(self):
         self.NameEntry_Var.set( self.curr_mbr.name )
-        self.IDEntry_Var.set( self.curr_mbr.mem_id )
-        self.PhoneEntry_Var.set( self.curr_mbr.cell_phone)
+        self.CellPhoneEntry_Var.set( self.curr_mbr.cell_phone )
+
+        self.PositionEntry_Var.set(     self.curr_mbr.cell_phone    )
+        self.NameEntry_Var.set(         self.curr_mbr.name          )
+        self.IDCardEntry_Var.set(       self.curr_mbr.cell_phone    )
+        self.BorthYearROCEntry_Var.set( self.curr_mbr.cell_phone    )
+        self.BorthYMDEntry_Var.set(     self.curr_mbr.cell_phone    )
+        self.AreaEntry_Var.set(         self.curr_mbr.cell_phone    )
+        self.CellPhoneEntry_Var.set(    self.curr_mbr.cell_phone    )
+        self.PhonePrimaryEntry_Var.set( self.curr_mbr.cell_phone    )
+        self.PhoneSecondEntry_Var.set(  self.curr_mbr.cell_phone    )
+        self.AddressEntry_Var.set(      self.curr_mbr.cell_phone    )
+
+    def GUI_blank_entry(self):
+        self.NameEntry_Var.set( "" )
+        self.CellPhoneEntry_Var.set( "" )
 
     def queryMember(self):
-        self.curr_mbr.load_from_db( int( self.QueryID_Var.get() ) )
-        self.NameEntry_Var.set( self.curr_mbr.name )
-        self.IDEntry_Var.set( self.curr_mbr.mem_id )
-        self.PhoneEntry_Var.set( self.curr_mbr.cell_phone )
+        self.curr_mbr.load_from_db( self.QueryID_Var.get() )
+
+        self.PositionEntry_Var.set(     self.curr_mbr.position      )
+        self.NameEntry_Var.set(         self.curr_mbr.name          )
+        self.IDCardEntry_Var.set(       self.curr_mbr.id_card       )
+        self.BorthYearROCEntry_Var.set( self.curr_mbr.birthdayROC   )
+        self.BorthYMDEntry_Var.set(     self.curr_mbr.birthday      )
+        self.AreaEntry_Var.set(         self.curr_mbr.area          )
+        self.CellPhoneEntry_Var.set(    self.curr_mbr.cell_phone    )
+        self.PhonePrimaryEntry_Var.set( self.curr_mbr.phone         )
+        self.PhoneSecondEntry_Var.set(  self.curr_mbr.phone2        )
+        self.AddressEntry_Var.set(      self.curr_mbr.address       )
 
     def SaveMemberInDB(self):
-        self.curr_mbr.name = str(self.NameEntry_Var.get())
-        self.curr_mbr.mem_id = int(self.IDEntry_Var.get())
-        self.curr_mbr.cell_phone = str(self.PhoneEntry_Var.get())
+        self.curr_mbr.position      = str( self.PositionEntry_Var.get()     )
+        self.curr_mbr.name          = str( self.NameEntry_Var.get()         )
+        self.curr_mbr.id_card       = str( self.IDCardEntry_Var.get()       )
+        self.curr_mbr.birthdayROC   = str( self.BorthYearROCEntry_Var.get() )
+        self.curr_mbr.birthday      = str( self.BorthYMDEntry_Var.get()     )
+        self.curr_mbr.area          = str( self.AreaEntry_Var.get()         )
+        self.curr_mbr.cell_phone    = str( self.CellPhoneEntry_Var.get()    )
+        self.curr_mbr.phone         = str( self.PhonePrimaryEntry_Var.get() )
+        self.curr_mbr.phone2        = str( self.PhoneSecondEntry_Var.get()  )
+        self.curr_mbr.address       = str( self.AddressEntry_Var.get()      )
 
         self.curr_mbr.save_to_db()
-
 
 def main():
     root = tk.Tk()
@@ -81,3 +202,12 @@ def main():
 if __name__ == '__main__':
     main()
 
+
+"""
+Reference
+
+1. Entry constructor and grid should be stated separatly
+   http://stackoverflow.com/questions/1101750/tkinter-attributeerror-nonetype-object-has-no-attribute-get
+2. Using lambda to call function instead handling by callback function
+   http://stackoverflow.com/questions/21943718/how-to-bind-the-enter-key-to-a-button-in-tkinter
+"""
--- member.py
+++ member.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 
 import sqlite3 as sql3
+import openpyxl as pyxl
 
 INVALID_ID = 65535
 INVALID_CARD_ID = '##########'
 INVALID_PHONE = '09__-______'
 
-DB_FILE_NAME = "running_club_member.db"
+DB_FILE_NAME            = "running_club_member.db"
+EXCEL_FILE_NAME         = "running_table.xlsx"
+EXCEL_FILE_TS_NAME      = "running_table_test.xlsx"
+EXCEL_SHEET_ALL_NAME    = "新會員(全)"
 
 class member:
     def __init__(self, **meminfo):
-        self.__name = meminfo.get('name', 'Unknown')                    # String
-        self.__mem_id = meminfo.get('memid', INVALID_ID)                # Unsigned Int
-        self.__cell_phone = meminfo.get('cell_phone', INVALID_PHONE )   # String
-        self.__position = meminfo.get('position', 'Unknown')            # String
-        self.__id_card = meminfo.get('idcard', INVALID_CARD_ID)         # String
-        self.__birthday = meminfo.get('birthday', '')                   # String, will revise as list later
-        self.__area = meminfo.get('area', '')                           # String
-        self.__address = meminfo.get('address', '')                     # String
-        self.__phone = meminfo.get('phone', '' )                        # String
-        self.__phone2 = meminfo.get('phone2', '' )                      # String
+        self.__name         = meminfo.get( 'name', 'Unknown'            )       # String
+        self.__mem_id       = meminfo.get( 'id', INVALID_ID             )       # Unsigned Int
+        self.__cell_phone   = meminfo.get( 'cell_phone', INVALID_PHONE  )       # String
+        self.__position     = meminfo.get( 'position', 'Unknown'        )       # String
+        self.__id_card      = meminfo.get( 'idcard', INVALID_CARD_ID    )       # String
+        self.__birthday     = meminfo.get( 'birthday', ''               )       # String, will revise as list later
+        self.__birthdayROC  = meminfo.get( 'birthdayROC', ''            )       # Unsigned Int
+        self.__area         = meminfo.get( 'area', ''                   )       # String
+        self.__address      = meminfo.get( 'address', ''                )       # String
+        self.__phone        = meminfo.get( 'phone', ''                  )       # String
+        self.__phone2       = meminfo.get( 'phone2', ''                 )       # String
 
         '''
         Create database if not exist
         '''
-        self.create_db()
+        self.create_tbl()
 
     @property
     def name(self):
@@ -38,6 +43,38 @@ class member:
     def cell_phone(self):
         return self.__cell_phone
 
+    @property
+    def position(self):
+        return self.__position
+
+    @property
+    def id_card(self):
+        return self.__id_card
+
+    @property
+    def birthday(self):
+        return self.__birthday
+
+    @property
+    def birthdayROC(self):
+        return self.__birthdayROC
+
+    @property
+    def area(self):
+        return self.__area
+
+    @property
+    def address(self):
+        return self.__address
+
+    @property
+    def phone(self):
+        return self.__phone
+
+    @property
+    def phone2(self):
+        return self.__phone2
+
     @name.setter
     def name(self, name):
         self.__name = name
@@ -50,14 +87,64 @@ class member:
     def cell_phone(self, cell_phone_number):
         self.__cell_phone = cell_phone_number
 
-    def create_db( self ):
+    @position.setter
+    def position(self, position):
+        self.__position = position
+
+    @id_card.setter
+    def id_card(self, id_card):
+        self.__id_card = id_card
+
+    @birthday.setter
+    def birthday(self, birthday):
+        self.__birthday = birthday
+
+    @birthdayROC.setter
+    def birthdayROC(self, birthdayROC):
+        self.__birthdayROC = birthdayROC
+
+    @area.setter
+    def area(self, area):
+        self.__area = area
+
+    @address.setter
+    def address(self, address):
+        self.__address = address
+
+    @phone.setter
+    def phone(self, phone):
+        self.__phone = phone
+
+    @phone2.setter
+    def phone2(self, phone2):
+        self.__phone2 = phone2
+
+    def xstr( self, s ):
+        if s is None:
+            return ''
+        return str(s)
+
+    def create_tbl( self ):
         with sql3.connect( DB_FILE_NAME ) as conn:
             c = conn.cursor()
             c.execute( ''' CREATE TABLE IF NOT EXISTS member (
                         id          INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+                        position    TEXT NOT NULL,
                         name        TEXT NOT NULL,
-                        cell_phone  TEXT NOT NULL
+                        idcard      TEXT,
+                        birthROC    INTEGER,
+                        birth       TEXT,
+                        area        TEXT,
+                        cell_phone  TEXT,
+                        phone       TEXT,
+                        phone2      TEXT,
+                        address     TEXT
                         )''' )
+
+    def drop_tbl( self ):
+        with sql3.connect( DB_FILE_NAME ) as conn:
+            c = conn.cursor()
+            c.execute( "DROP TABLE member")
 
     def load_from_db( self, mem_id ):
         with sql3.connect( DB_FILE_NAME ) as conn:
@@ -65,14 +152,80 @@ class member:
             c.execute( "SELECT * FROM member WHERE id=?", (mem_id, ) )
             result = c.fetchone()
             if result != None:
-                self.__mem_id = result[0]
-                self.__name = result[1]
-                self.__cell_phone = result[2]
+                self.__mem_id       = result[0]
+                self.__position     = result[1]
+                self.__name         = result[2]
+                self.__id_card      = result[3]
+                self.__birthdayROC  = result[4]
+                self.__birthday     = result[5]
+                self.__area         = result[6]
+                self.__cell_phone   = result[7]
+                self.__phone        = result[8]
+                self.__phone2       = result[9]
+                self.__address      = result[10]
             else:
                 print("Fetch nothing in DB")
 
     def save_to_db( self ):
         with sql3.connect( DB_FILE_NAME ) as conn:
             c = conn.cursor()
-            c.execute( "INSERT INTO member VALUES (?, ?, ?)", ( self.__mem_id, self.__name, self.__cell_phone ) )
+            c.execute( '''INSERT INTO member
+                          ( position, name, idcard, birthROC, birth, area, cell_phone, phone, phone2, address )
+                          VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )''',
+                          ( self.__position,
+                            self.__name,
+                            self.__id_card,
+                            self.__birthdayROC,
+                            self.__birthday,
+                            self.__area,
+                            self.__cell_phone,
+                            self.__phone,
+                            self.__phone2,
+                            self.__address ) )
 
+    def cnvt_excel_to_db( self ):
+        """
+        Load excel file and specific the sheet
+        """
+        wb = pyxl.load_workbook( EXCEL_FILE_NAME )
+        sheet_all = wb.get_sheet_by_name( EXCEL_SHEET_ALL_NAME )
+
+        """
+        Delete and recreate the table of database
+        """
+        self.drop_tbl()
+        self.create_tbl()
+
+        with sql3.Connection( DB_FILE_NAME ) as conn:
+            c = conn.cursor()
+            for item in sheet_all.iter_rows( row_offset = 1 ):
+                if item[0].value != None:
+                    print(int(item[0].value))
+                    c.execute( '''INSERT INTO member
+                                  ( position, name, idcard, birthROC, birth, area, cell_phone, phone, phone2, address )
+                                  VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )''',
+                                  ( self.xstr( item[1].value ),
+                                    self.xstr( item[2].value ),
+                                    self.xstr( item[3].value ),
+                                    self.xstr( item[4].value ),
+                                    self.xstr( item[5].value ),
+                                    self.xstr( item[6].value ),
+                                    self.xstr( item[7].value ),
+                                    self.xstr( item[8].value ),
+                                    self.xstr( item[9].value ),
+                                    self.xstr( item[10].value ) ) )
+
+    def cnvt_db_to_excel( self ):
+        wb = pyxl.Workbook()
+        for sheet in wb.worksheets:
+            wb.remove_sheet( sheet )
+
+        ws = wb.create_sheet( title=EXCEL_SHEET_ALL_NAME )
+        ws.append( ["編號", "職稱", "姓名", "身分證", "年次", "出生年月日", "地址", "手機", "電話1", "電話2", "通訊處(地址)"] )
+        with sql3.Connection( DB_FILE_NAME ) as conn:
+            c = conn.cursor()
+            c.execute( "SELECT * FROM member" )
+            for row in c:
+                ws.append( [ x for x in row ] )
+
+        wb.save( filename = EXCEL_FILE_TS_NAME )
